5f4fd0972f7afe7c14d99edfa539c56d61149c1f
gabrielvieiraferreira <101935758+gabrielvieiraferreira@users.noreply.github.com>
2023-09-29 01:09:48 -0300

    Add files via upload

---
+++ TP02.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool is_prime(int num) {
+    if (num < 2)
+        return false;
+
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+
+void find_prime_sum_decomposition(int num) {
+    if (num <= 2) {
+        printf("O número deve ser maior que 2 e par.\n");
+        return;
+    }
+
+    printf("Decomposição de %d como a soma de dois números primos:\n", num);
+
+    for (int i = 2; i <= num / 2; i++) {
+        if (is_prime(i) && is_prime(num - i)) {
+            printf("%d + %d\n", i, num - i);
+            return;
+        }
+    }
+
+    printf("Não foi possível encontrar uma decomposição.\n");
+}
+
+int main() {
+    int num;
+
+    printf("Digite um número inteiro par maior que 2: ");
+    scanf("%d", &num);
+
+    find_prime_sum_decomposition(num);
+
+    return 0;
+}
+
